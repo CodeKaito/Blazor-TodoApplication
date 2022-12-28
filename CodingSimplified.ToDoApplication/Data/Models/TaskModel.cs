cf298b94f9676035eaf23e2a53db4499d7133380
Jerome Decinco <57111980+CodeKaito@users.noreply.github.com>
2022-12-28 10:59:08 +0100

    Add files via upload

---
+++ CodingSimplified.ToDoApplication/Data/Models/TaskModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CodingSimplified.ToDoApplication.Data.Models
+{
+    public class TaskModel
+    {
+        public string TaskName { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
